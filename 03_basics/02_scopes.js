608cf0eba4158dae99ddf83b405c14285cc766d0
nikitap2906 <nikitapatidar9068@gmail.com>
2025-09-28 18:50:17 +0530

    scope this and arrow

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,10 +12,47 @@ if (true) {
     let a = 10;
     const b = 20;
     // var c = 30;
-    console.log("Inner :", a);
+    // console.log("Inner :", a);
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one() {
+    const username = "nikita"
+
+    function two() {
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if (true) {
+    const username = "nikita";
+    if (username === "nikita") {
+        const website = " youtube";
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+function addone(num) {
+    return num + 1
+}
+// addone(5);
+
+// addTwo(5);
+const addTwo = function (num) {
+    return num + 2
+}
+addTwo(5);
