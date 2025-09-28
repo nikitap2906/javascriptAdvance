9a5a01bd9245d1f3024f3e5d537b4fb93651bd0f
nikitap2906 <nikitapatidar9068@gmail.com>
2025-09-28 17:52:38 +0530

    delete unnaccessary folder

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,21 @@
+// ******** Global Scop *******
+
+
+// let a = 10;     
+// const b = 20;
+// var c = 300;
+let a = 300;
+
+//  ******* Blok Scop {} *******
+
+if (true) {
+    let a = 10;
+    const b = 20;
+    // var c = 30;
+    console.log("Inner :", a);
+}
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
